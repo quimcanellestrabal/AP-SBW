c98b9c35ac1d4a77d8aed600e101125d73d293ad
nuaquilue <nuaquilue@gmail.com>
2023-12-06 17:12:58 +0100

    rename scripts harvest.area() intens.def.curr() sbw.outbreak() apread.tonew()

---
+++ R/sbw.outbreak.R
@@ -0,0 +1,206 @@
+
+#@ exemple:: load("data/landscape.rda"); land=landscape; preoutbreak=1; outbreak=1; calm=1; collapse=1; duration.last.outbreak=1
+
+sbw.outbreak = function(land, params, tbls, preoutbreak=1, outbreak=1, calm=1, collapse=1, 
+                        duration.last.outbreak=1, mask=NA){
+
+  # 0.  Fix function  
+  `%notin%` = Negate(`%in%`)
+
+  ## Determine cell resolution in km
+  cell.size.km = (mask@extent[2] - mask@extent[1])/ncol(mask) / 10^3
+  
+  ## Check that weights make sense
+  if(params$w.wind>1){
+    stop("Weight of wind factor cannot be greater than 1")
+  }
+  if(params$w.host>1){
+    stop("Weight of neighbor host factor cannot be greater than 1")
+  }
+  
+  ## 1. Defliation in the different phases of the SBW outbreak
+  cat("   Defoliation in the ")
+  if(preoutbreak>0){
+    cat("pre-epidemic phase ", "\n")
+    # preoutbreak=5
+    
+    #Potential són les cells en què hi pot haver sbw. Tota la funció s'aplica a "potential" i no a "land"
+    potential = filter(land, ny.def0>=5, tssbw>=30, spp %in% c("SAB", "EPN"), temp>0.5, temp<2.8)
+    
+    #Primers epicentres
+    sbw.new.sprd = sample(potential$cell.id, size=rdunif(1,4,7-preoutbreak), replace=F,
+                          prob=potential$ny.def0*(1200-potential$elev)/100)
+ 
+    ## find between 20 to 40 neighs and add to sbw.new.sprd
+    ## @nú: make a for so each epicenter core has a different size indicated by the k parameter!
+    neighs = nn2(dplyr::select(land,x,y), filter(land, cell.id %in% sbw.new.sprd) %>% dplyr::select(x,y),
+                 k=rdunif(1,20,40) , searchtype='priority')  #test ##neighs sobre "land" perquè són el màxim des cells on SBW podria anar independenment de la disponibilitat prevista a "potential"
+    nn.indx = neighs[[1]]
+    sbw.new.sprd = land$cell.id[nn.indx]
+    sbw.new.sprd = unique(sbw.new.sprd)
+    
+    # just give some intensity to the core of the epicenters
+    land$curr.intens.def[land$cell.id %in% sbw.new.sprd] = 
+      sample(0:3, size=length(sbw.new.sprd), replace=T, prob=c(0.2,0.4,0.3,0.1))
+    
+    # add some neighs of these epicenters #Quim radi a 8 (mida original = 12)
+    sbw.new.sprd = c(sbw.new.sprd, 
+                     spread.tonew(land, nc=ncol(mask), side=cell.size.km, 
+                                  radius=8, outbreak, preoutbreak))
+    sbw.new.sprd = unique(sbw.new.sprd)
+    
+    #Select sbw.new.sprd only on potential cells
+    potential = filter(land, spp %in% c("SAB", "EPN"))
+    sbw.new.sprd = sbw.new.sprd[sbw.new.sprd %in% potential$cell.id]
+    
+    # and finally assign intensity to all of them (rewrite intensity just assigned to epicenter cores)
+    land$curr.intens.def[land$cell.id %in% sbw.new.sprd] = 
+      sample(0:3, size=length(sbw.new.sprd), replace=T, prob=c(0.2,0.4,0.3,0.1)) ###COMPROVAR AQUESTES PROPORCIONS!
+  }
+
+  if(outbreak>0){
+    cat("epidemic phase ", "\n")
+    ## Spatial spreading of the current outbreak to cells not yet defoliated, that is, cells with ny.def0>=5 & tssbw>=30
+    ## The function 'spread.tonew' returns cell.ids
+    ## The radius has to be variable to allow spreading further away or limit outbreak
+    radius = rdunif(1, params$radius.outbreak.mid-params$radius.outbreak.range, params$radius.outbreak.mid+params$radius.outbreak.range) 
+    sbw.new.sprd = spread.tonew(land, nc=ncol(mask), side=cell.size.km, radius=radius, outbreak, preoutbreak,
+                                params$w.wind, params$w.host, params$reduc.nnew.outbreak, params$reduc.nnew.preoutbreak)
+    sbw.new.sprd = unique(sbw.new.sprd)
+    
+    ## Only if some new cells are defoliated, assign level of defoliation
+    if(length(sbw.new.sprd)>0){
+      ## Select sbw.new.sprd only on potential cells
+      potential = filter(land, spp %in% c("SAB", "EPN"))
+      sbw.new.sprd = sbw.new.sprd[sbw.new.sprd %in% potential$cell.id]
+      
+      ## Level of defoliation of the cells recently integrated in the outbreak (the sbw.new.spread cells)
+      ## It can be 0 (no-defoliation), 1, 2 or 3!
+      land$curr.intens.def[land$cell.id %in% sbw.new.sprd] = 
+        sample(0:3, size=length(sbw.new.sprd), replace=T, prob=c(0.2,0.4,0.3,0.1)) 
+    }
+  }
+
+  if(collapse>0){ 
+    cat("collapse phase ", "\n")
+    ## if collapse, reduce number of new cells, only spontaneously
+    potential = filter(land, ny.def0>=5, tssbw>=30, spp %in% c("SAB", "EPN"),temp>0.5, temp<2.8)
+    sbw.new.sprd = sample(potential$cell.id, size=round(rlnorm(1, 2,1.5)), replace=F,  #mn(lnorm) ~ ifelse(collapse==1, 2, 1.5)
+                          prob=potential$ny.def0*(1200-potential$elev)/100)
+    
+    # # #Some spontaneous new plots
+    # if(rdunif(1,1,3)==1){
+    #   radius = rdunif(1,6,12+collapse) #smaller radius than outbreak phase
+    #   sbw.new.sprd = spread.tonew(land, nc=ncol(mask), side=2000/10^3,radius=radius, outbreak, preoutbreak)
+    #   sbw.new.sprd = unique(sbw.new.sprd)
+    #   #Select sbw.new.sprd only onn potential cells
+    #   sbw.new.sprd = sbw.new.sprd[sbw.new.sprd %in% potential$cell.id]
+    # }
+    
+    #Add intensity
+    land$curr.intens.def[land$cell.id %in% sbw.new.sprd] = 
+      sample(0:2, size=length(sbw.new.sprd), replace=T, prob=c(0.5,0.3,0.2))
+  }
+
+  if(calm>0){    #calm>0 | collapse>0
+    cat("calm phase ", "\n")
+    ## not add new locations to the current outbreak if it is collapsing
+    ## when calm, few spontaneously cells will have to be here and there defoliated
+    potential = filter(land, ny.def0>=5, tssbw>=30, spp %in% c("SAB", "EPN"),temp>0.5, temp<2.8)
+    sbw.new.sprd = sample(potential$cell.id, size=round(rlnorm(1, 2,1.5)), replace=F,  #mn(lnorm) ~ ifelse(collapse==1, 2, 1.5)
+                          prob=potential$ny.def0*(1200-potential$elev)/100)
+    #Add intensity
+    land$curr.intens.def[land$cell.id %in% sbw.new.sprd] = 
+      sample(0:2, size=length(sbw.new.sprd), replace=T, prob=c(0.5,0.3,0.2))
+  }
+  
+  
+  ## 2. Update SBW tracking variables for newly defoliated cells
+  land$ny.def[land$cell.id %in% sbw.new.sprd] = 
+    ifelse(land$curr.intens.def[land$cell.id %in% sbw.new.sprd]==0, 0, 1)
+  land$ny.def0[land$cell.id %in% sbw.new.sprd] = 
+    ifelse(land$curr.intens.def[land$cell.id %in% sbw.new.sprd]>0, 0, 
+           land$ny.def0[land$cell.id %in% sbw.new.sprd]+1)
+  land$cum.intens.def[land$cell.id %in% sbw.new.sprd] = 
+    land$curr.intens.def[land$cell.id %in% sbw.new.sprd]
+  
+  
+  ## 3. Update level of defoliation intensity of already defoliated cells, that is, 
+  ## cells with ny.def0<5 & tssbw>=30, and ny.def<=18 --> otherwise, the defoliation is forced to stop.
+  sbw.curr.sprd = land$cell.id[land$ny.def0<5 & land$tssbw>=30 & land$ny.def<=18 & 
+                                 land$cell.id %notin% sbw.new.sprd]
+  curr.outbreak = filter(land, cell.id %in% sbw.curr.sprd)
+  ## Level of defoliation of these cells. It can be 0 (no-defol), 1, 2 or 3!
+  land$curr.intens.def[land$cell.id %in% sbw.curr.sprd] = 
+    intens.def.curr(filter(land, cell.id %in% sbw.curr.sprd), params, tbls, preoutbreak, outbreak, collapse, calm)
+  ## Update SBW tracking variables
+  land$ny.def[land$cell.id %in% sbw.curr.sprd] = land$ny.def[land$cell.id %in% sbw.curr.sprd]+
+    ifelse(land$curr.intens.def[land$cell.id %in% sbw.curr.sprd]==0, 0, 1)
+  land$ny.def0[land$cell.id %in% sbw.curr.sprd] = 
+    ifelse(land$curr.intens.def[land$cell.id %in% sbw.curr.sprd]>0, 0, 
+           land$ny.def0[land$cell.id %in% sbw.curr.sprd]+1)
+  land$cum.intens.def[land$cell.id %in% sbw.curr.sprd] = 
+    land$cum.intens.def[land$cell.id %in% sbw.curr.sprd]+land$curr.intens.def[land$cell.id %in% sbw.curr.sprd]  
+  
+  
+  ## 4. Stop defoilating cells that have been defoliated for more than 18 years
+  ## Warning, not consider cells that have just been defoliated
+  ## And avoid recurrence of defoliation in this oubreak by making tssbw==0
+  sbw.stop = land$cell.id[land$ny.def0==0 & land$ny.def>18 & land$cell.id %notin% sbw.curr.sprd]
+  land$curr.intens.def[land$cell.id %in% sbw.stop] = 0
+  land$ny.def0[land$cell.id %in% sbw.stop] = 1
+  land$tssbw[land$cell.id %in% sbw.stop] = 0
+  
+  
+  ## 5. Increase number of year of non-defoliation for the remaing cells
+  land$ny.def0[land$cell.id %notin% c(sbw.new.sprd, sbw.curr.sprd, sbw.stop)] = 
+    land$ny.def0[land$cell.id %notin% c(sbw.new.sprd, sbw.curr.sprd, sbw.stop)]+1
+  
+  
+  ## 6. For those cells that have just accumulated 5 years of non-defoliation, 
+  ## reset the counter of number of years of defoliation and 
+  ## cumulative intensity of defoliaion
+  land$ny.def[land$ny.def0==5] = 0
+  land$cum.intens.def[land$ny.def0==5] = 0
+  
+  
+  ## 7. Kill cells according to number of years of defoliation and species composition
+  ## with probability as cumulative*current intensity of defoliation
+  if(outbreak>0 | collapse>0){
+    kill.cells = forest.mortality(land)
+  }
+  else{
+    kill.cells = integer()
+  }
+  
+  
+  ## 8. Set outbreak parameters
+  if(preoutbreak>0){ # pre-epidemic
+    preoutbreak = preoutbreak-1
+    phase = "preoutbreak"
+    if(preoutbreak==0)
+      duration.last.outbreak = outbreak = rdunif(1,0,2)+params$duration.last.outbreak  # +6 Gray2008 #Quim: ho he canviat (outbreak entre 9 i 11), l'original era "rdunif(1,-1,2) (outbreak entre 8 i 10"
+  }
+  else if(outbreak>0){ # epidemia
+    outbreak = outbreak-1
+    phase = "outbreak"
+    if(outbreak==0)
+      collapse = rdunif(1,4,6) #Original entre 3 i 6
+  }
+  else if(collapse>0){  # collapse
+    phase = "collapse"
+    collapse = collapse-1
+    if(collapse==0) #finishing the collapse
+      calm = round(rnorm(1, 15, 2))
+  }
+  else if(calm>0){
+    phase = "calm"
+    calm = calm-1    
+    if(calm==0)
+      preoutbreak = rdunif(1,3,4)
+  }
+  
+  res = list(kill.cells=kill.cells, land.sbw=land, preoutbreak=preoutbreak, outbreak=outbreak, collapse=collapse, calm=calm)
+  return(res) 
+} 
+
---
+++ R/spread.tonew.r
@@ -0,0 +1,45 @@
+spread.tonew = function(land, nc, side, radius, outbreak, preoutbreak, w.wind, w.host,
+                        reduc.nnew.outbreak, reduc.nnew.preoutbreak){
+  
+  # cat("SBW adjacent spreading", "\n" )
+  
+  ## Function to simulate spreading to cells not yet defoliated, that is, cells with ny.def0>=5 and tssbw>=30
+  ## The future function sbw.spread.tonew(filter(land, ny.def0>=5)) will return a vector with cell.ids
+  ## MB:  Prob.spread in cell c = Proportion host species in neighborhood x 
+  ##      level of defoliation in neighborhood during previous year x some climatic variable 
+  ##      (and perhaps wind direction in future versions)
+  ## The question is, how many cells do I have to select from the pool of potential cells?
+  ## It should be a number of cells proportional to the number of cells defoliated in the previous year¿?
+
+  ## Compute neighborhood current defoliation and neighborhood host preference
+  potential = neigh.influence.sbw.spread(land, nc, side, radius)
+  
+  ## The probability of sbw spread into a cell is proportional to neigh.curr.def, neigh.host.pref and 
+  ## dominant wind directions
+  ## Rescale the variables to the range [0,1] before applying any weight
+  potential$x = w.wind * 0 + w.host * rescale(potential$neigh.host.pref, to=c(0,1)) +
+    (1-pmax(w.wind+w.host,1)) * rescale(potential$neigh.curr.def, to=c(0,1))
+  
+  ## Select only those cells that at least one neighbor is defoliated
+  potential = potential[potential$neigh.curr.def>0 & potential$x>0,]
+  if(nrow(potential)>1){
+    if(outbreak<=10){  ## consolidated part of the outbreak
+      #nnew = pmin(nrow(potential), sum(na.omit(land$curr.intens.def)>0)*runif(1, 0.15, 0.45)) #valors originals
+      # nnew = pmin(nrow(potential), sum(na.omit(land$curr.intens.def)>0)*runif(1, 0.6, 1)) #valors meus perquè nnew sigui 10000-30000 de SAB i EPN
+      num.new = nrow(potential) * (1-runif(1, 0, reduc.nnew.outbreak))
+    }
+    if(preoutbreak>0 | outbreak>10){  
+      #nnew = pmin(nrow(potential), sum(na.omit(land$curr.intens.def)>0)*runif(1, 0.75, 1)) #valors originals
+      # nnew = pmin(nrow(potential), sum(na.omit(land$curr.intens.def)>0)*runif(1, 1, 1.5))
+      num.new = nrow(potential) * (1-runif(1, 0, reduc.nnew.preoutbreak))
+    }
+    sbw.new.sprd = sample(potential$cell.id, round(num.new), replace=F, prob=potential$x)
+  }
+  else if(nrow(potential)==1){
+    sbw.new.sprd = potential$cell.id
+  } else{
+    sbw.new.sprd = integer()
+  }
+    
+  return(sbw.new.sprd)
+}
