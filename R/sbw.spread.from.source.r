0971feb2b734a9f8585952594efa89a1643506b6
nuaquilue <nuaquilue@gmail.com>
2024-05-05 07:49:31 +0200

    wind factor in spread.from.source

--- R/sbw.spread.from.source.r
+++ R/sbw.spread.from.source.r
@@ -5,8 +5,7 @@
 ## C. The position of the target cell with respect with the main wind direction
 ##########################################################################################
 
-
-sbw.spread.from.source = function(land, nc, radius=12){
+sbw.spread.from.source = function(land, nc, radius=12, side = 1){
 
   ## Select all the cells that are currently defoliated  
   source.cells = land[land$ny.def>0,]
@@ -31,9 +30,6 @@ sbw.spread.from.source = function(land, nc, radius=12){
   r = dist[,radius]
   weight_dist = round(exp(-dist^2/(r/2)^2),3)
   linear_dist = round((r-dist)/r,3)
-  head(weight_dist)
-  head(linear_dist)
-  
   
   ## ---> Criteria A
   ## Matrix with the cell.id of the neighbor cells that are not defoliated yet
@@ -46,10 +42,11 @@ sbw.spread.from.source = function(land, nc, radius=12){
   
   ## Relation between the source cell, the target cell andthe species of the source cell
   source_wspp = pivot_longer(neigh_nodefol, cols=2:(radius), values_to="target") %>% select(-name) %>% 
-    filter(target!=0) %>% left_join(select(land, cell.id, spp), by=c("source" = "cell.id")) 
+    filter(target!=0) %>% left_join(select(land, cell.id, spp), by=c("source" = "cell.id")) %>% 
+    rename(source_spp = spp)
   
   ## Apply the weight corresponding to these species
-  source_wspp$w_spp = ifelse(source_spp$spp %in% c("EPN", "SAB"), 0.7, 0.3)
+  source_wspp$w_spp = ifelse(source_wspp$source_spp %in% c("EPN", "SAB"), 0.7, 0.3)
   
   ## ---> Criteria B
   ## Matrix with the distance of the neighbor cells that are not defoliated yet
@@ -63,18 +60,15 @@ sbw.spread.from.source = function(land, nc, radius=12){
   ## Relation between the source cell and the distance with the target cells (not explicitly indicated in this data frame,
   ## but we keep the order of them)
   source_dist =  pivot_longer(neigh_dist, cols=2:(radius), values_to="dist") %>% select(-name)  %>% filter(dist!=0)
-  r = max(source_dist$dist)-2000; r
+  r = max(source_dist$dist)-2000
   
   ## Apply the weight corresponding to the distance
   source_dist$w_dist = exp(-(source_dist$dist-2000)^2/(r/2)^2)
     
   ## ---> Criteria C  
   ## Élise wheel !  19/04/2024
-  ## Questions
-  # Do we need to turn the wheel according to a user-defined prevailing wind?
-  # We won't have a raster with the prevailing wind in each cell (we assume, too complex)
-  nc =  804
-  side = 1
+  ## Do we need to turn the wheel according to a user-defined prevailing wind?
+  ## We won't have a raster with the prevailing wind in each cell (we assume, too complex
   x <- c(radius:-radius)
   G <- expand.grid(x,x)
   names(G) <- c("x","y")
@@ -91,26 +85,24 @@ sbw.spread.from.source = function(land, nc, radius=12){
   # else G$w_angle = (360 - G$shift)/180
   G$w_angle <- abs(sign(G$shift-180))*((1/2)*(1-sign(G$shift-180))*((180 - G$shift)/180) + (1/2)*(1+sign(G$shift-180))*((360 - G$shift)/180))
   
-  ## The position of each cell are in: G$z is like ids - ids[,1]
-  kk = ids[1,]
-  positions = kk - kk[1]
-  filter(G, z %in% positions)
-  
-  positions = ids - ids[,1]
-  G$z
-  
-  source_wind ---
+  ## The position of each cell are in: G$z. And G$z is like position = ids[i,] - ids[i,1] (for the source cell 'i')
+  source_wind = source_wspp %>% select(source, target) %>% mutate(position=target-source) %>% 
+    left_join(select(G, z, w_angle), by=c("position"="z"))
     
   ## Merge the three criteria and compute the final weight 
-  res = cbind(source_wspp, source_dist[,-1], source_wind[,-1]) %>% mutate(w=w_spp*w_dist*w_angle) %>%  
-    group_by(target) %>% summarise(final_w=sum(w)) #, totw=sum(w_dist))
-  res$host.influence = res$final_w/max(res$final_w)
-      #(res$final_w-min(res$final_w))/(max(res$final_w)-min(res$final_w))
-   # rescaling (x ) /(xmax - xmin)
-    
-  ## Rescale the variables to the range [0,1] before applying any weight
- 
-    
+  res = cbind(source_wspp, source_dist[,-1], source_wind[,"w_angle"]) %>% mutate(w=w_spp*w_dist*w_angle) %>%  
+    group_by(target) %>% summarise(final_w=sum(w)) 
+  
+  ## Rescaling the final weight to [0,1]
+  res$spread.potential = (res$final_w-min(res$final_w))/(max(res$final_w)-min(res$final_w)) 
+  
+  ## Questions:  
+  ## Rescale the variables to the range [0,1] before applying any weight ??
+  ## Do we want to apply a weight to each of these factors?
+  ## Do we use a multiplicative or an additive formula?
+  ## weight_factor_spp*w_spp + weight_factor_distance*w_dist + weight_factor_angel*w_angle
+  
+  return(res)  
 }
 
 
